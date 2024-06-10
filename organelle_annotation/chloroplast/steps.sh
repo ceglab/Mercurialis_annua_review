9700b8521dece37cc0be0e4ed0be5cbc5e3c3527
ceglab <52042219+ceglab@users.noreply.github.com>
2024-06-10 14:08:48 +0530

    Update steps.sh

--- organelle_annotation/chloroplast/steps.sh
+++ organelle_annotation/chloroplast/steps.sh
@@ -1,1 +1,3 @@
-
+1) The chloroplast genome was downloaded from NCBI here: https://www.ncbi.nlm.nih.gov/nuccore/OX359234.1/
+2) The genome was annotated using GeSeq - Annotation of Organellar Genomes
+3) The settings used to perform the annotation are documented in the pdf file GESEQ_settings_Chloroplast.pdf.
